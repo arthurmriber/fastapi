af4690c653cb2adc8bd95dfe429aa486b4265f0f
Arthur M. Ribeiro <156614501+arthurmriber@users.noreply.github.com>
2025-09-02 11:34:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI, Request
-from routers import getnews # Importa as rotas
+from routers import getnews
 from routers import filter
+from routers import inference
+from routers import analyze
 from routers import search
+from routers import searchterm
+from routers import inference_createposter
+from routers import cronjob
 
 # Instancia a aplicação FastAPI
 app = FastAPI()
-
+    
 @app.get("/")
 def greet_json():
     return {"Hello": "World!"}
@@ -13,4 +18,9 @@ def greet_json():
 # Inclui as rotas
 app.include_router(getnews.router)
 app.include_router(filter.router)
+app.include_router(inference.router)
+app.include_router(analyze.router)
 app.include_router(search.router)
+app.include_router(searchterm.router)
+app.include_router(inference_createposter.router)
+app.include_router(cronjob.router)
